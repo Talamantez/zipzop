2907d7b1d0356a9c0d9e52f89e2c8f11f2c955fe
Talamantez <robert.talamantez2@gmail.com>
2015-01-26 12:11:43 -0800

    merging aspirations page with bootstrap template

---
+++ src/components/aspirations-page/aspirations.js
@@ -0,0 +1,37 @@
+define(["knockout", "text!./aspirations.html"], function(ko, aspirationsTemplate) {
+
+  function AspirationsViewModel(route) {
+    this.message = ko.observable('Enter an aspiration:');
+    this.aspiration = ko.observable();
+  }
+
+  AspirationsViewModel.prototype.doSomething = function() {
+    this.message('You invoked doSomething() on the viewmodel.');
+  };
+
+
+  return { viewModel: AspirationsViewModel, template: aspirationsTemplate };
+
+});
+
+
+/*function AppViewModel() {	
+    this.firstName = "Bert";
+    this.lastName = "Bertington";
+
+    this.firstName = ko.observable("bert");
+    this.lastName = ko.observable("berto");
+    
+    this.fullName = ko.computed(function(){
+        return this.firstName() + " " + this.lastName();
+    }, this);
+    
+    this.capitalizeLastName = function() {
+        var currentVal = this.lastName();        // Read the current value
+        this.lastName(currentVal.toUpperCase()); // Write back a modified value
+    };
+}
+
+
+// Activates knockout.js
+ko.applyBindings(new AppViewModel());*/
