f393a4d0b974928b6fb0b24ea96461c0459a5ad5
Talamantez <robert.talamantez2@gmail.com>
2015-01-27 14:50:20 -0800

    disabled nav-bar manipulations

--- src/js/custom.js
+++ src/js/custom.js
@@ -16,14 +16,14 @@
 
 	
 	//jQuery to collapse the navbar on scroll
-	$(window).scroll(function() {
+/*	$(window).scroll(function() {
 		if ($(".navbar").offset().top > 50) {
 			$(".navbar-fixed-top").addClass("top-nav-collapse");
 		} else {
 			$(".navbar-fixed-top").removeClass("top-nav-collapse");
 		}
 	});
-	
+	*/
 	//jQuery for page scrolling feature - requires jQuery Easing plugin
 	$(function() {
 		$('.navbar-nav li a').bind('click', function(event) {
