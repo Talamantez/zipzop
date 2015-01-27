6c2946cea4f1f065f30e62c18550572f46b5637d
Talamantez <robert.talamantez2@gmail.com>
2015-01-27 13:19:51 -0800

    foreach working, using unshift in place of push to reverse array formation

--- src/components/aspirations-page/aspirations.js
+++ src/components/aspirations-page/aspirations.js
@@ -1,26 +1,47 @@
 define(["knockout", "text!./aspirations.html"], function(ko, aspirationsTemplate) {
 
-  var Aspiration = function(content){
+/*  var Aspiration = function(content){
     self=this;
     self.content = content;
   }
+*/
+
+// Enter key binding handler
+ko.bindingHandlers.enterkey = {
+    init: function (element, valueAccessor, allBindingsAccessor, viewModel) {
+        var allBindings = allBindingsAccessor();
+        $(element).keypress(function (event) {
+            var keyCode = (event.which ? event.which : event.keyCode);
+            if (keyCode === 13) {
+                allBindings.enterkey.call(viewModel);
+                return false;
+            }
+            return true;
+        });
+    }
+};
 
   function AspirationsViewModel(route) {
     var self = this;
+    
     self.message = ko.observable('What do you ASPIRE to do?');
+    
     self.aspiration = ko.observable();
-    self.aspirations = ko.observableArray([
-        new Aspiration('Build a sustainable future for humanity')
-      ]);
+    
+    self.aspirations = ko.observableArray([]);
 
     self.addAspiration = function(content){
-        console.log('adding aspiration');
-        self.aspirations.push(new Aspiration(self.aspiration()));
-        console.log(self.aspirations());
+        // console.log('adding aspiration: ' + self.aspiration() );
+        self.aspirations.unshift( self.aspiration() );
+        // console.log( self.aspirations() );
+        self.aspiration('');
       }
+    
+    self.removeAspiration = function(seat){ self.aspirations.remove(aspiration)}
   }
 
   AspirationsViewModel.prototype.someFunction = function() {
+
   };
 
   return { viewModel: AspirationsViewModel, template: aspirationsTemplate };
