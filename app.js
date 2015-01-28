55272017a1f7772cf46a325ad7378566e0a67470
Talamantez <robert.talamantez2@gmail.com>
2015-01-27 16:49:56 -0800

    enough to show

---
+++ app.js
@@ -0,0 +1,54 @@
+// dependencies
+var fs = require('fs');
+var http = require('http');
+var express = require('express');
+//var routes = require('./routes');
+var path = require('path');
+var mongoose = require('mongoose');
+
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+// global config
+var app = express();
+app.set('port', process.env.PORT || 1337);
+app.use(express.logger());
+app.use(express.favicon());
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.cookieParser('your secret here'));
+app.use(express.session());
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// env config
+app.configure('development', function(){
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+    app.use(express.errorHandler());
+});
+
+// passport config
+
+var Account = require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+
+// mongo config
+
+mongoose.connect('mongodb://localhost/passport_local_mongoose');
+
+// mongo model
+// var Model_Name = require('add_your_models_here');
+
+/*// routes
+require('./routes')(app);
+*/
+// run server
+app.listen(app.get('port'), function(){
+  console.log('\nExpress server listening on port ' + app.get('port'));
+});
